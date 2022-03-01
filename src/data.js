cdc137520925e480cf18c574897560e0dad7f722
Zitan Salehin <zitan-74@dipti.com.bd>
2022-03-02 01:21:04 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default  [
+    {
+        id:1,
+        name:'James John',
+        age: 29,
+        image: 'https://images.ctfassets.net/u4vv676b8z52/7lGpjVsLwhHFbyv98C28Vk/e1b1b89e69f11d70d8fb5643090b3014/square-face-round-glasses-660x440-compressor.jpg?fm=jpg&q=80',
+    },
+    {
+        id:2,
+        name:'Linda Smith',
+        age: 27,
+        image: 'https://media.allure.com/photos/5ac3b8f9424f773b2007db4d/1:1/w_2000,h_2000,c_limit/best-glasses-for-your-face-shape-hannah-1.jpg',
+    },
+    {
+        id:3,
+        name:'Ashley Prince',
+        age: 33,
+        image: 'https://i2.wp.com/www.hadviser.com/wp-content/uploads/2019/04/29-pixie-cut-with-highlights-and-bangs-BXaXhICgKs_.jpg',
+    },
+    {
+        id:4,
+        name:'Shiv Datt',
+        age: 30,
+        image: 'https://media.istockphoto.com/photos/daylight-portrait-of-young-handsome-caucasian-man-isolated-on-grey-picture-id1025855432?k=20&m=1025855432&s=612x612&w=0&h=m7ZCxng4HOxL9zgkTTaIPaixIaETUzaK3jGaqnFO3cM=',
+    },
+    {
+        id:5,
+        name:'Jona James',
+        age: 23,
+        image: 'https://images.unsplash.com/photo-1572460418264-695212ed493a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OXx8fGVufDB8fHx8&w=1000&q=80',
+    },
+]
